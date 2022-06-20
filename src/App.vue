edd10bb2fb2b9012e6478458c5d9c501110110ec
MomentRing <904372358@qq.com>
2022-06-20 21:26:30 +0800

    初始化配置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div id="app">
+    <h1>黑马头条</h1>
+    <div class="box"></div>
+    <i class="toutiao toutiao-shipin"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
     <router-view />
   </div>
 </template>
@@ -8,4 +21,10 @@
 export default {}
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.box {
+  width: 375px;
+  height: 100px;
+  background-color: pink;
+}
+</style>
